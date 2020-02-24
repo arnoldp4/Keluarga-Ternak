0eba5b5e0c2b311a71c7c5b64912fc262eb3ad66
arnoldp4 <almaware44@gmail.com>
2020-02-24 23:33:15 +0700

    Gameplay Improvement #4.5
    - Diorama telah dibuat dan masih belum bisa ke Panel Baru

---
+++ Assets/Script/DioramaManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DioramaManager : MonoBehaviour
+{
+    public GameObject Ayam, Sapi, Domba, Anjing, Kucing, Beruang, Mole;
+    public Text TitleEncycloTxt, EncycloTxt;
+    Image tumpangsprite;
+    Sprite gambarSekarang;
+    public Image gambarnow;
+    
+
+    public void CekEncyclo(string namahewan){
+        if(namahewan == "Anjing"){
+            tumpangsprite = gameObject.GetComponent<Image>();
+            gambarSekarang = tumpangsprite.sprite;
+            TitleEncycloTxt.text = "Anjing";
+            EncycloTxt.text = "Anjing adalah hewan mamalia yang memiliki indra penciuman yang sangat tajam. Banyak dari kita memelihara anjing. Selain mudah dijumpai, anjing juga dapat menjadi hewan penjaga rumah serta menjadi kawan untuk bermain.";
+        }
+    }
+    public void UbahGambar(){
+        gambarnow.sprite = gambarSekarang;
+    }
+}
