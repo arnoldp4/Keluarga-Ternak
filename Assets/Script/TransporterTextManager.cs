ec7b86bc3aa41cba3cacce01b34c5f5e5f7eeafb
arnoldp4 <almaware44@gmail.com>
2020-03-01 23:41:35 +0700

    Gameplay Short Improvement #2
    TransporterTextManager:
     - Diberi tambahan max agar pemain dapat tahu berapa banyak jumlah barang yg bisa dijual

--- Assets/Script/TransporterTextManager.cs
+++ Assets/Script/TransporterTextManager.cs
@@ -39,7 +39,7 @@ public class TransporterTextManager : MonoBehaviour
         TepungTxt.text = "Tepung: " + TransporterManager.jualtepung; CatTxt.text = "Ember Cat: " + TransporterManager.jualcat;
         PancakeTxt.text = "Pancake: " + TransporterManager.jualpancake; PizzaTxt.text = "Pizza: " + TransporterManager.jualpizza;
         BajuTxt.text = "Baju: " + TransporterManager.jualbaju;
-        ApplySellTxt.text = "Total Jual: " + TransporterManager.totaljual;
+        ApplySellTxt.text = "Total Jual: " + TransporterManager.totaljual + " [Max: " + LevelGameplay.UpgradeTransporter + "]";
     }
 
     void LoadingDelivery(){
